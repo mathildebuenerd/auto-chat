fc4bc1a2c637a4ccd97a9eec6a0c195713ed51eb
mathildebuenerd <mathilde.buenerd@gmail.com>
2017-12-13 11:46:27 +0100

    autocompletion fonctionne mais avec une lettre de décalage à chaque fois

--- public/autocomplete.js
+++ public/autocomplete.js
@@ -1,77 +1,86 @@
 let listOfWords = [];
 
 let sayInput = document.querySelector('.inputSay');
-sayInput.addEventListener('keypress', (e) => {
-    console.log(e);
-    let key = e.key;
-    let allowedLetters = new RegExp('[a-z, ,\',Backspace]', 'gi');
-    if (allowedLetters.exec(key)) { // on ne prend en compte que les lettres et l'espace
-        console.log("letter allowed!");
 
-        let query = '';
-        if (key === 'Backspace') {
-            query = (sayInput.value).slice(0, -1);
-        } else {
-            query = sayInput.value + key;
-        }
+setup();
 
-        console.log(query);
-        fillAutocompleteOptions(query);
-    }
-});
+function setup() {
 
-function fillAutocompleteOptions(query) {
-
-    // s'il y a plusieurs mots, ne se base que sur le dernier
-    let espace = new RegExp('\\s', 'g'); // caractère blanc, s'il y en a au moins un c'est que l'on a plus d'un mot
-    if (espace.exec(query) !== null) {
-        let lastWord = findLastWord(query);
-        function findLastWord(query) {
-            let i = (query.length)-1;
-            let finished = false;
-            // /while (i>=0) {
-                if (espace.exec(query) !== null) {
-                    return i;
+    // on regarde quand l'utilisateur appuie sur la touche espace pour faire une proposition en fonction du dernier mot en train d'être tapé et pas de l'ensemble de la chaîne de caractère
+    sayInput.addEventListener('keypress', (e) => {
+        // console.log(e);
+        let key = e.key;
+        console.log(key);
+        let query = sayInput.value + key;
+        let allowedLetters = new RegExp('[a-z, ,\',Space,Backspace]', 'gi');
+        let lastSpaceIndex = findLastSpaceIndex(query); // on récupère l'index du dernier espace pour pouvoir slicer correctement le dernier mot
+        console.log("lastSpaceIndex " + lastSpaceIndex);
+        function findLastSpaceIndex(query) {
+            for (let i=query.length; i>0; i--) {
+                if(query[i] === " ") {
+                    return i+1; // on renvoie l'index qui correspond à la lettre qui se situe juste après l'espace
                 }
-                i--;
-            // }
-            console.log(i);
-        }
-        for (let i=(query.length)-1; i>=0; i--) { // on part de la dernière lettre et on s'arrête quand on rencontre un espace
-            console.log('1 query : ' + query[i]);
-
+            }
+            return 0;
         }
 
-    }
-    // console.log("query " + query);
+        if (allowedLetters.exec(key)) { // on ne prend en compte que les lettres et l'espace
+            console.log("letter allowed! " + key);
 
-    let matchedWords = [];
-    for (let i=0; i<listOfWords.length; i++) {
-        if(listOfWords[i].indexOf(query) !== -1) { // si le pattern match, on ajoute le mot dans le tableau
-            // console.log("j'ai trouvé une correspondance! " + listOfWords[i]);
-            matchedWords.push(listOfWords[i]);
+            if (key === "Backspace") {
+                key = '';
+            }
+            console.log("sayInput value " + sayInput.value);
+            let lastWord = (sayInput.value).substring(lastSpaceIndex) + key; // on ajoute 1 à lastSpaceIndex pour ne pas compter l'espace
+
+            console.log("lastWord " + lastWord);
+            console.log('key ' + key);
+            fillAutocompleteOptions(lastWord);
+            function fillAutocompleteOptions(query) {
+
+                console.log("query dans fillautocomplete " + query);
+
+                let matchedWords = [];
+                for (let i=0; i<listOfWords.length; i++) {
+                    if(listOfWords[i].indexOf(query) !== -1) { // si le pattern match, on ajoute le mot dans le tableau
+                        // console.log("j'ai trouvé une correspondance! " + listOfWords[i]);
+                        matchedWords.push(listOfWords[i]);
+                    }
+                }
+
+                let suggestions = document.querySelectorAll('.suggestion');
+
+                // Si aucun mot ne correspond, on rempli avec d'autres mots
+                while (matchedWords.length < 3) {
+                    let randomIndex = Math.floor(Math.random()*listOfWords.length);
+                    matchedWords.push(listOfWords[randomIndex]);
+                    // console.log("matched words");
+                    // console.log(matchedWords);
+                }
+
+
+                for (let i=0; i<suggestions.length; i++) { // on parcoure le nombre de suggestions
+                    // let randomIndex = Math.floor(Math.random()*matchedWords.length);
+                    suggestions[i].textContent = matchedWords[i];
+                }
+
+                console.log("suggestions");
+                console.log(matchedWords);
+
+
+            }
         }
-    }
+    });
 
-    let suggestions = document.querySelectorAll('.suggestion');
-
-    while (matchedWords.length <= 10) {
-        let randomIndex = Math.floor(Math.random()*listOfWords.length);
-        matchedWords.push(listOfWords[randomIndex]);
-        // console.log("matched words");
-        // console.log(matchedWords);
-    }
-
-
-    for (let i=0; i<suggestions.length; i++) { // on parcoure le nombre de suggestions
-        let randomIndex = Math.floor(Math.random()*matchedWords.length);
-        suggestions[i].textContent = matchedWords[randomIndex];
-    }
-
+    addAutocompleteOptions(3);
+    fillAutocompleteWithJson('data');
 
 }
+
+
+
+
 // getWords();
-addAutocompleteOptions(3);
 
 
 // change toutes les options toutes les 3 secondes
@@ -129,8 +138,6 @@ function addAutocompleteOptions(quantity) { // d'abord on crée un nombre de pla
     }
 
 }
-
-fillAutocompleteWithJson('data');
 function fillAutocompleteWithJson(jsonFile) {
 
     $.getJSON(jsonFile, (json) => {
